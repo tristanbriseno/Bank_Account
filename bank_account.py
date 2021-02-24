a2bd54dcf38c510c0d115ae7a4dfa960d8185f47
Tristan_Briseno <tristan.k.briseno@gmail.com>
2021-02-24 17:05:29 -0500

    addeed the deposit and withdrw function

--- bank_account.py
+++ bank_account.py
@@ -1,4 +1,11 @@
+from random import *
+#imports ra ndom
 
+def new_acc_num():
+    return randrange(10000000, 99999999)
+    #creates an account number
+    
+     
     class BankAccount:
 
     #Provided BankAccount with attributes
@@ -8,3 +15,21 @@
         self.account_num=new_acc_num()
         self.routing_num=routing_num
         self.balance=balance
+
+
+
+         #this is the deposit function
+    def deposit(self, amount):
+        self.balance += amount
+        print(f"You have deposited: ${amount}")
+
+
+
+    #this is the withdraw function
+    def withdraw(self, amount):
+        if amount > self.balance:
+            print("You have Insufficient Funds! you will be charged an overdraft fee of: $10")
+            self.balance -= 10
+        else:
+            self.balance -= amount
+            print(f"Your have withdrawn: ${amount}")
