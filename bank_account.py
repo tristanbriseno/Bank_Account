b491d0af04c34249b04690484a2a5595c523db07
Tristan_Briseno <tristan.k.briseno@gmail.com>
2021-02-24 16:39:43 -0500

    importing random for the random account number.

--- bank_account.py
+++ bank_account.py
@@ -1,8 +1,10 @@
-from random import *
-#imports ra ndom
 
-def new_acc_num():
-    return randrange(10000000, 99999999)
-    #creates an account number
+    class BankAccount:
 
+    #Provided BankAccount with attributes
+    def __init__(self, full_name, routing_num, balance=0):
     
+        self.full_name=full_name
+        self.account_num=new_acc_num()
+        self.routing_num=routing_num
+        self.balance=balance
